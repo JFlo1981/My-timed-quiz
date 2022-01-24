ed4e0144fb003002e140c424b9c060c32835610e
JFlo1981 <jarod.florence@outlook.com>
2022-01-24 13:10:47 -0600

    pseudocode writtern in JS file

---
+++ assets/js/quizScript.js
@@ -0,0 +1,9 @@
+// add const variables for timer element, question element, answer choices, finl score, initials element, high scores, and random question shufle
+// add let variables for timer, score, question and final score
+// create function to move thru divs
+// start game function to starttimer and queue first question
+// set up timer function
+// set up question cyle function
+// setup corrct vs incorrect
+// set up end quiz and score display
+// set up initals entry logged in local storage
