2961a0a79b7ddf6184407ef4dabf28312c3e8e12
Jarod Florence <jarod.florence@outlook.com>
2022-05-01 20:09:27 -0500

    question.js with shuffle created

--- assets/js/questionList.js
+++ assets/js/questionList.js
@@ -1,48 +1,64 @@
-// array of questions to be shuffled for random order
+// array of questions/answers to be shuffled for random order
 const questionList = [
     {
-        question: '',
+        question: 'HTML',
         answerArray: [
-            { answer: '', correct: true }, // correct answer
-            { answer: '', correct: false },
-            { answer: '', correct: false },
-            { answer: '', correct: false },
+            { answer: 'What is responsible for the structure of a web page?', correct: true }, // correct answer
+            { answer: 'What is responsible for the style of a web page?', correct: false },
+            { answer: 'What type of file ends with the ".css" suffix?', correct: false },
+            { answer: 'What type of file ends with the ".js" suffix?', correct: false },
         ]
     },
     {
-        question: '',
+        question: 'Console.log();',
         answerArray: [
-            { answer: '', correct: true }, // correct answer
-            { answer: '', correct: false },
-            { answer: '', correct: false },
-            { answer: '', correct: false },
+            { answer: 'What tool can print content to the Inspector for debugging?', correct: true }, // correct answer
+            { answer: 'What tool can find all instances of a word in a code set?', correct: false },
+            { answer: 'What command terminates a function?', correct: false },
+            { answer: 'What command logs data to local storage?', correct: false },
         ]
     },
     {
-        question: '',
+        question: 'Concat',
         answerArray: [
-            { answer: '', correct: true }, // correct answer
-            { answer: '', correct: false },
-            { answer: '', correct: false },
-            { answer: '', correct: false },
+            { answer: 'What is a method for combining string data from two strings and returning a single string?', correct: true }, // correct answer
+            { answer: 'What is a method for removing a portion of a string?', correct: false },
+            { answer: 'What method adds to an array?', correct: false },
+            { answer: 'What animal will try to take money from you with elaborate schemes?', correct: false },
         ]
     },
     {
-        question: '',
+        question: 'Github',
         answerArray: [
-            { answer: '', correct: true }, // correct answer
-            { answer: '', correct: false },
-            { answer: '', correct: false },
-            { answer: '', correct: false },
+            { answer: 'What is a website for hosting code in repositories?', correct: true }, // correct answer
+            { answer: 'What is a command line interface program for manipulating data?', correct: false },
+            { answer: 'What is a programming language originally known as Ecmascript?', correct: false },
+            { answer: 'What is a local storage device on all computers?', correct: false },
         ]
     },
     {
-        question: '',
+        question: 'Boolean',
         answerArray: [
-            { answer: '', correct: true }, // correct answer
-            { answer: '', correct: false },
-            { answer: '', correct: false },
-            { answer: '', correct: false },
+            { answer: 'What is a data type that expects a "true/false" value?', correct: true }, // correct answer
+            { answer: 'What is a data type consisting of characters in quotes?', correct: false },
+            { answer: 'What is the element at the top of every HTML file? ', correct: false },
+            { answer: 'What is a "parameter" better known as?', correct: false },
         ]
     }
 ];
+
+function shuffleQuesAndAnswers() {
+    let newQuestionList = questionList;
+    // shuffle answer order
+    for (let i = 0; i < newQuestionList.length; i++) {
+        let answerArray = newQuestionList[i].answerArray;
+        answerArray.sort(() => {
+            return (Math.floor(Math.random() * 3) - 1);
+        });
+    }
+    // shuffle question order
+    newQuestionList = newQuestionList.sort(() => {
+        return (Math.floor(Math.random() * 3) - 1);
+    });
+    return newQuestionList;
+};
